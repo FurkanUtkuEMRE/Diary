5466edcaa48ce0a116be51234ad8173403811ab9
Furkan Utku EMRE <furkanutkuemre@hotmail.com>
2024-08-30 00:55:16 +0300

    Finalized CRUD.

--- DiaryApp/Controllers/DiaryEntriesController.cs
+++ DiaryApp/Controllers/DiaryEntriesController.cs
@@ -24,5 +24,76 @@ namespace DiaryApp.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(DiaryEntryModel diaryEntry)
+        {
+            ValidateInput(diaryEntry);
+
+            if (ModelState.IsValid)
+            {
+                _db.DiaryEntries.Add(diaryEntry);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(diaryEntry);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            DiaryEntryModel? diaryEntry = _db.DiaryEntries.Find(id);
+
+            if (diaryEntry == null)
+            {
+                return NotFound();
+            }
+
+            return View(diaryEntry);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(DiaryEntryModel diaryEntry)
+        {
+            ValidateInput(diaryEntry);
+
+            if (ModelState.IsValid)
+            {
+                _db.DiaryEntries.Update(diaryEntry);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(diaryEntry);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            DiaryEntryModel? diaryEntry = _db.DiaryEntries.Find(id);
+
+            if (diaryEntry == null)
+            {
+                return NotFound();
+            }
+
+            _db.DiaryEntries.Remove(diaryEntry);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public void ValidateInput(DiaryEntryModel diaryEntry)
+        {
+            if (diaryEntry.Title == null || diaryEntry.Content == null || diaryEntry.Date == DateTime.MinValue)
+            {
+                ModelState.AddModelError("Title", "You need to give the entry a title.");
+                ModelState.AddModelError("Content", "You need to write some content for the entry.");
+                ModelState.AddModelError("Date", "You need to pick a date for your entry.");
+            }
+            else if (diaryEntry.Title.Length < 3 && diaryEntry.Content.Length < 5)
+            {
+                ModelState.AddModelError("Title", "Your title must be between 3 and 50 characters!");
+                ModelState.AddModelError("Content", "Your content must be between 5 and 50 characters!");
+            }
+        }
     }
 }
--- DiaryApp/Models/DiaryEntryModel.cs
+++ DiaryApp/Models/DiaryEntryModel.cs
@@ -7,13 +7,15 @@ namespace DiaryApp.Models
         [Key]
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "You need to give the entry a title.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Your title must be between 3 and 50 characters!")]
         public required string Title { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "You need to write some content for the entry.")]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Your title must be between 5 and 50 characters!")]
         public required string Content { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "You need to pick a date for your entry.")]
         public DateTime Date { get; set; } = DateTime.Now;
     }
 }
